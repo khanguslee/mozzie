f41868f371e35dee098d72ad12b1903b2e7f8854
khanguslee <khanguslee@gmail.com>
2020-01-12 16:19:04 +1100

    Transpile React using webpack into a dist folder

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <p>mozzie</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = [
   {
     mode: 'development',
@@ -13,8 +15,32 @@ module.exports = [
       ],
     },
     output: {
-      path: __dirname + '/src',
+      path: __dirname + '/dist',
       filename: 'main.js',
     },
+  },
+  {
+    mode: 'development',
+    entry: './src/App.tsx',
+    target: 'electron-renderer',
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          include: /src/,
+          use: [{ loader: 'ts-loader' }],
+        },
+      ],
+    },
+    output: {
+      path: __dirname + '/dist',
+      filename: 'app.js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+    ],
   },
 ];
